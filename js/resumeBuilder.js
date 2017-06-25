5d2caa0206488d7776dab731afa173364b64b7d0
Ashish Upadhyay <ashishupadhyay855@gmail.com>
2017-06-25 19:53:28 +0530

    done

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,31 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+//var formattedName= HTMLheaderName.replace("%data%",name);
+//var name = "ashish";
+//var role="webdeveloper"
+//var formattedRole= HTMLheaderRole.replace("%data%",role);
+//$("#header").append(formattedRole);
+//$("#header").prepend(formattedName);
+var skills = ["awesomeness","programming"];
+$("#main").append(skills);
+$("#main").append(skills[0]);
+$("#main").append(skills.lenght);
+
+var bio={
+  "name":"ashish",
+  "role":"webdeveloper",
+  "contacts":{
+    "mobile":"0123456789","email":john@example.com,"location":"india"
+  },
+  "welcomemessage":"shagagjglshlfka",
+  "skills":["awesomeness","programming"],
+}
+//$("#main").append(bio.name)
+var work ={};
+work.position="course developer";
+work.employer = "udacity";
+work.years=0.3;
+var education = {};
+education ["name"]="mumbai university";
+education["years"]="2005-2006"
+education["city"]= "mumbai";
+$("#main").append (work["position"]);
