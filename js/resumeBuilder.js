92178dab16d7df50242f63ab8c59760a20ec4363
Ashish Upadhyay <ashishupadhyay855@gmail.com>
2017-06-25 21:49:37 +0530

    my third quiz

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,3 +5,7 @@ var formattedName= HTMLheaderName.replace("%data%","ashish");
 var formattedRole= HTMLheaderRole.replace("%data%","web developer");
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+var skills = ["awesomeness","programming"];
+$("#main").append(skills);
+$("#main").append(skills[0]);
+$("#main").append(skills.lenght);
